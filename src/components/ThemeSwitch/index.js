8eda27f3369b7743be1a4a1353731830deae0918
DavidKadaria <david.qada@gmail.com>
2024-03-31 23:24:35 +0400

    Implemented ThemeSwitch component and its styles (for mobile devices)

--- src/components/ThemeSwitch/index.js
+++ src/components/ThemeSwitch/index.js
@@ -1,11 +1,31 @@
-// import { getTheme, setTheme } from '../../utils';
+import { useCallback, useEffect } from 'react';
+import { getTheme, checkThemeValidity, setTheme } from '../../utils';
+import { IconMoon } from '../../icons';
 
 import './ThemeSwitch.css';
 
 function ThemeSwitch() {
+	const handleThemeChange = useCallback(() => {
+		const currentTheme = getTheme();
+		const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+
+		setTheme(newTheme);
+	}, []);
+
+	useEffect(() => {
+		const currentTheme = getTheme();
+
+		if (!checkThemeValidity(currentTheme)) {
+			setTheme('light');
+		}
+	}, []);
+
 	return (
-		<div className='ThemeSwitch'>
-			<button className='ThemeSwitch__button'>Switch theme</button>
+		<div className='ThemeSwitch' onClick={handleThemeChange}>
+			<button className='ThemeSwitch__button'>
+				<span className='ThemeSwitch__wheel' />
+			</button>
+			<IconMoon />
 		</div>
 	);
 }
