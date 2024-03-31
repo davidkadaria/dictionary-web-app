49b7aa4cc77d8319a8c2f811004eebde8ea4d375
DavidKadaria <david.qada@gmail.com>
2024-04-01 03:12:01 +0400

    Added Main to the render function of the root component (App.js)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { Header } from './components';
+import { Header, Main } from './components';
 
 function App() {
 	return (
 		<div className='container'>
 			<Header />
+			<Main />
 		</div>
 	);
 }
