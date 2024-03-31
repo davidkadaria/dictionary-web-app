cb8d23ae7f34e3bdb42296ff6ce29f3a16a23643
DavidKadaria <david.qada@gmail.com>
2024-03-31 21:29:33 +0400

    Added ThemeSwitch component and styles for it

---
+++ src/components/ThemeSwitch/index.js
@@ -0,0 +1,13 @@
+// import { getTheme, setTheme } from '../../utils';
+
+import './ThemeSwitch.css';
+
+function ThemeSwitch() {
+	return (
+		<div className='ThemeSwitch'>
+			<button className='ThemeSwitch__button'>Switch theme</button>
+		</div>
+	);
+}
+
+export { ThemeSwitch };
