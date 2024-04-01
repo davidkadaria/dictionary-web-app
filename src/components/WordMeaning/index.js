a7da304623ed40878997791ec312e944cd10864b
DavidKadaria <david.qada@gmail.com>
2024-04-02 02:42:29 +0400

    Implemented WordMeaning component's JSX

--- src/components/WordMeaning/index.js
+++ src/components/WordMeaning/index.js
@@ -1,7 +1,27 @@
+import { RelatedWords } from '../';
+
 import './WordMeaning.css';
 
-function WordMeaning() {
-	return <></>;
+function WordMeaning({ data }) {
+	return (
+		<div className='WordMeaning'>
+			<h2 className='WordMeaning__part-of-speech'>{data.partOfSpeech}</h2>
+			<h3 className='WordMeaning__heading'>Meaning</h3>
+			<ul className='WordMeaning__definitions'>
+				{data.definitions.map((definition, index) => (
+					<li key={index} className='WordMeaning__definition'>
+						{definition.definition}
+					</li>
+				))}
+			</ul>
+			{data.synonyms && data.synonyms.length > 0 && (
+				<RelatedWords heading='Synonyms' words={data.synonyms} />
+			)}
+			{data.antonyms && data.antonyms.length > 0 && (
+				<RelatedWords heading='Antonyms' words={data.antonyms} />
+			)}
+		</div>
+	);
 }
 
 export { WordMeaning };
