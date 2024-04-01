d72ea6a159d27493e5367f238313e9ce56688e6c
DavidKadaria <david.qada@gmail.com>
2024-04-01 04:50:06 +0400

    Modified the Main component: added submit handler, results state, and also modified its JSX content

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,7 +1,43 @@
+import { useState, useRef } from 'react';
+import { fetchWordInformation } from '../../utils';
+
+import { SearchForm } from '../';
+
 import './Main.css';
 
 function Main() {
-	return <main>main</main>;
+	const [results, setResults] = useState();
+	const [validationError, setValidationError] = useState(false);
+
+	const searchFieldRef = useRef();
+
+	async function handleSubmit(event) {
+		event.preventDefault();
+		const searchFieldValue = searchFieldRef.current.value;
+
+		// Validate search field
+		if (!searchFieldValue || searchFieldValue.trim() === '') {
+			setValidationError(true);
+			return;
+		} else if (validationError) {
+			setValidationError(false);
+		}
+
+		// Fetch word information
+		fetchWordInformation(searchFieldValue).then((data) => {
+			setResults(data);
+		});
+	}
+
+	return (
+		<main className='Main'>
+			<SearchForm
+				handleSubmit={handleSubmit}
+				hasValidationError={validationError}
+				ref={searchFieldRef}
+			/>
+		</main>
+	);
 }
 
 export { Main };
