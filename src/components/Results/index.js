8fdae45e58529982f3f867175a3c42b6f2081010
DavidKadaria <david.qada@gmail.com>
2024-04-02 03:17:41 +0400

    Updated Results component and its styles (including media queries)

--- src/components/Results/index.js
+++ src/components/Results/index.js
@@ -5,7 +5,10 @@ import { IconPlay } from '../../icons';
 import './Results.css';
 
 function Results({ data }) {
-	const [play] = useSound(data.phonetics.find((phonetic) => phonetic.audio).audio || '');
+	const audio = data.phonetics.find(
+		(phonetic) => phonetic.audio && phonetic.audio.length > 5 // Some audio files are empty or contain only a few bytes of dummy data
+	).audio;
+	const [play] = useSound(audio);
 
 	return (
 		<section className='Results'>
@@ -14,7 +17,10 @@ function Results({ data }) {
 					<h1 className='Results__word'>{data.word}</h1>
 					<p className='Results__word-phonetic'>{data.phonetic}</p>
 				</div>
-				<div className='Results__audio' onClick={play}>
+				<div
+					className={`Results__audio${!audio ? ' Results__audio--disabled' : ''}`}
+					onClick={play}
+				>
 					<IconPlay />
 				</div>
 			</div>
