aa4a4fd504635e973d3cd37a4062dbf36391f999
DavidKadaria <david.qada@gmail.com>
2024-04-01 04:49:23 +0400

    Implemented SearchForm component (including stylings)

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,14 +1,33 @@
 import { forwardRef } from 'react';
+import { IconSearch } from '../../icons';
 
 import './SearchForm.css';
 
 const SearchForm = forwardRef(function SearchForm(props, ref) {
-	const { handleSubmit } = props;
+	const { handleSubmit, hasValidationError } = props;
 
 	return (
-		<form onSubmit={handleSubmit}>
-			<input type='text' ref={ref} />
-			<button>Search</button>
+		<form
+			className={`SearchForm${hasValidationError ? ' SearchForm--error' : ''}`}
+			onSubmit={handleSubmit}
+		>
+			<div
+				className='SearchForm__field-wrapper'
+				onClick={() => {
+					ref.current.focus();
+				}}
+			>
+				<input
+					className='SearchForm__input'
+					type='text'
+					placeholder='Search for any word…'
+					ref={ref}
+				/>
+				<button className='SearchForm__button'>
+					<IconSearch />
+				</button>
+			</div>
+			{hasValidationError && <p className='SearchForm__error'>Whoops, can't be empty…</p>}
 		</form>
 	);
 });
