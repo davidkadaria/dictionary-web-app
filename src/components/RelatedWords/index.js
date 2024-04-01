e46ef65511a46fa001a42ce2adda5440b2173021
DavidKadaria <david.qada@gmail.com>
2024-04-02 02:43:20 +0400

    Implemented RelatedWords component's JSX

--- src/components/RelatedWords/index.js
+++ src/components/RelatedWords/index.js
@@ -1,7 +1,18 @@
 import './RelatedWords.css';
 
-function RelatedWords() {
-	return <></>;
+function RelatedWords({ heading, words }) {
+	return (
+		<div className='RelatedWords'>
+			<h3 className='RelatedWords__heading'>{heading}</h3>
+			<div className='RelatedWords__words'>
+				{words.map((word) => (
+					<span className='RelatedWords__word' key={word}>
+						{word}
+					</span>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export { RelatedWords };
