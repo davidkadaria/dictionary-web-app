7b3967d7c51f21c1080a8d27c02dac7aab824249
DavidKadaria <david.qada@gmail.com>
2024-04-02 03:17:00 +0400

    Updated Play icon's color, now it's dynamically defined based on parent element's color property

--- src/icons/index.js
+++ src/icons/index.js
@@ -51,8 +51,8 @@ function IconNewWindow() {
 function IconPlay() {
 	return (
 		<svg xmlns='http://www.w3.org/2000/svg' width='75' height='75' viewBox='0 0 75 75'>
-			<g fill='#A445ED' fillRule='evenodd'>
-				<circle cx='37.5' cy='37.5' r='37.5' opacity='.25' />
+			<g fill='currentColor' fillRule='evenodd'>
+				<circle cx='37.5' cy='37.5' r='37.5' />
 				<path d='M29 27v21l21-10.5z' />
 			</g>
 		</svg>
