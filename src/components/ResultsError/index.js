6803376bcb1867896d66f2d7c29c65c7305d6f9e
DavidKadaria <david.qada@gmail.com>
2024-04-02 22:22:21 +0400

    Implemented ResultsError component and its styles (including media queries)

--- src/components/ResultsError/index.js
+++ src/components/ResultsError/index.js
@@ -1,7 +1,13 @@
 import './ResultsError.css';
 
-function ResultsError() {
-	return <></>;
+function ResultsError({ data }) {
+	return (
+		<div className='ResultsError'>
+			<div className='ResultsError__emoji'>😕</div>
+			<h2 className='ResultsError__title'>{data.title}</h2>
+			<p className='ResultsError__message'>{`${data.message} ${data.resolution}`}</p>
+		</div>
+	);
 }
 
 export { ResultsError };
