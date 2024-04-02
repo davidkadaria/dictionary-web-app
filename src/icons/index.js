375a58b84621ecbe118a2d9229323a4fc420de27
DavidKadaria <david.qada@gmail.com>
2024-04-02 21:19:02 +0400

    Modified new window icon. Now it inherits color from the parent element

--- src/icons/index.js
+++ src/icons/index.js
@@ -38,7 +38,7 @@ function IconNewWindow() {
 		<svg xmlns='http://www.w3.org/2000/svg' width='14' height='14' viewBox='0 0 14 14'>
 			<path
 				fill='none'
-				stroke='#838383'
+				stroke='currentColor'
 				strokeLinecap='round'
 				strokeLinejoin='round'
 				strokeWidth='1.5'
