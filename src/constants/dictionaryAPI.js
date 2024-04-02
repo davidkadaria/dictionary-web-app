5dea5e5d41f76a0d6efbbc576c99d254a45b962e
DavidKadaria <david.qada@gmail.com>
2024-04-02 22:21:11 +0400

    Updated dictionaryAPI constants. Now it only provides a single default Error results

--- src/constants/dictionaryAPI.js
+++ src/constants/dictionaryAPI.js
@@ -1,19 +1,9 @@
 const mainEndpoint = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
-const errorMessages = {
-	notFound: {
-		id: 404,
-		error: true,
-		shortDescription: 'No definitions found',
-		longDescription:
-			"Sorry pal, we couldn't find definitions for the word you were looking for. You can try the search again at later time or head to the web instead.",
-	},
-	error: {
-		id: 500,
-		error: true,
-		shortDescription: 'Server error',
-		longDescription:
-			"Sorry pal, we couldn't fetch the definitions for the word you were looking for. You can try the search again at later time or head to the web instead.",
-	},
+const defaultErrorResults = {
+	error: true,
+	title: 'Oops! Something went wrong.',
+	message: "Sorry pal, we couldn't fetch the definitions for the word you were looking for.",
+	resolution: 'You can try the search again at later time or head to the web instead.',
 };
 
-export { mainEndpoint, errorMessages };
+export { mainEndpoint, defaultErrorResults };
