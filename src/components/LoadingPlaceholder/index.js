a29a187afa7963a263c8377cf56f30766be11543
DavidKadaria <david.qada@gmail.com>
2024-04-02 21:47:24 +0400

    Implemented LoadingPlaceholder component and its styles (including media queries)

--- src/components/LoadingPlaceholder/index.js
+++ src/components/LoadingPlaceholder/index.js
@@ -1,7 +1,26 @@
+import { memo } from 'react';
+
 import './LoadingPlaceholder.css';
 
-function LoadingPlaceholder() {
-	return <></>;
-}
+const dummyContent = Array.from({ length: 5 }, (_, index) => index + 1);
+
+const LoadingPlaceholder = memo(function LoadingPlaceholder() {
+	return (
+		<div className='LoadingPlaceholder'>
+			<div className='LoadingPlaceholder__header'>
+				<div className='LoadingPlaceholder__word'>
+					<div className='LoadingPlaceholder__header__title'></div>
+					<div className='LoadingPlaceholder__header__subtitle'></div>
+				</div>
+				<div className='LoadingPlaceholder__header__audio'></div>
+			</div>
+			<div className='LoadingPlaceholder__content'>
+				{dummyContent.map((item) => (
+					<div className='LoadingPlaceholder__content__meaning' key={item}></div>
+				))}
+			</div>
+		</div>
+	);
+});
 
 export { LoadingPlaceholder };
