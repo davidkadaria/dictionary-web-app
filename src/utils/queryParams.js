dfbc97dfee35fb2ad4c09e016cd20afc8f9fe60f
DavidKadaria <david.qada@gmail.com>
2024-04-02 20:11:16 +0400

    Modified query param updation approach

--- src/utils/queryParams.js
+++ src/utils/queryParams.js
@@ -6,13 +6,13 @@ const getQueryParam = (key) => {
 const setQueryParams = (key, value) => {
 	const queryParams = new URLSearchParams(window.location.search);
 	queryParams.set(key, value);
-	window.history.replaceState({}, '', `${window.location.pathname}?${queryParams}`);
+	window.history.pushState({}, '', `${window.location.pathname}?${queryParams}`);
 };
 
 const removeQueryParam = (key) => {
 	const queryParams = new URLSearchParams(window.location.search);
 	queryParams.delete(key);
-	window.history.replaceState({}, '', `${window.location.pathname}?${queryParams}`);
+	window.history.pushState({}, '', `${window.location.pathname}?${queryParams}`);
 };
 
 export { getQueryParam, setQueryParams, removeQueryParam };
